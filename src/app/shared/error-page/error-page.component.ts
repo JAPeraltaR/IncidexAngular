26b26cb75c6fcbba7130d0a8b5d66260abc19f45
Jean <test04061994@gmail.com>
2025-02-18 15:55:29 +0100

    Only Structure

--- src/app/shared/error-page/error-page.component.ts
+++ src/app/shared/error-page/error-page.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'shared-error-page',
-  imports: [],
+  imports: [ MatCardModule, MatButtonModule ],
   templateUrl: './error-page.component.html',
   styleUrl: './error-page.component.css'
 })
 export class ErrorPageComponent {
+
+  private router = inject(Router)
+
+  goHome(){
+    this.router.navigateByUrl('')
+  }
 
 }
